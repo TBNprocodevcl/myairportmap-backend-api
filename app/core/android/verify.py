9519c1d10190970693038aa8287c5bd10867ba93
Thanh Le <thanhle09t2bkdn@gmail.com>
2026-04-20 07:19:54 +0700

    fix:load google

--- app/core/android/verify.py
+++ app/core/android/verify.py
@@ -3,15 +3,23 @@ from googleapiclient.discovery import build
 from app.core.config import settings
 
 SCOPES = ["https://www.googleapis.com/auth/androidpublisher"]
-
-credentials = service_account.Credentials.from_service_account_file(
-    settings.GOOGLE_SERVICE_ACCOUNT_FILE,
-    scopes=SCOPES
-)
+USE_GOOGLE = settings.USE_GOOGLE 
+if not USE_GOOGLE:
+    credentials = service_account.Credentials.from_service_account_file(
+        settings.GOOGLE_SERVICE_ACCOUNT_FILE,
+        scopes=SCOPES
+    )
+else:
+    credentials = None
 
 service = build("androidpublisher", "v3", credentials=credentials)
 
 def verify_android_subscription(package_name, product_id, purchase_token):
+    if not credentials:
+        return {
+            "purchaseState": 0,
+            "expiryTimeMillis": 9999999999999
+        }
     result = service.purchases().subscriptions().get(
         packageName=package_name,
         subscriptionId=product_id,
