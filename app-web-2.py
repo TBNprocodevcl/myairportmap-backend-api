351c8cffc886c80402441ad0a74d5490e2c88aa0
Thanh Le <thanhle09t2bkdn@gmail.com>
2026-04-25 21:01:12 +0700

    fix:deploy

--- app-web-2.py
+++ app-web-2.py
@@ -17016,4 +17016,5 @@ if __name__ == "__main__":
         load_data()
     except Exception as e:
         print("!! Startup data load error:", repr(e))
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
