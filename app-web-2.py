7238b5a6681a14be4305da8e06641440255f29c3
Thanh Le <thanhle09t2bkdn@gmail.com>
2026-05-13 18:30:12 +0700

    add:wellknown

--- app-web-2.py
+++ app-web-2.py
@@ -323,6 +323,10 @@ app = Flask(__name__)
 
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
 
+@app.route("/.well-known/<path:filename>")
+def well_known(filename):
+    return send_from_directory("well-known", filename)
+
 app.register_blueprint(auth_flask_bp)
 app.register_blueprint(airports_flask_bp)
 app.register_blueprint(achievements_flask_bp)
